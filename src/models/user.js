81c7109131b7fff8e503464548860bedea94a702
NehaGupta-12 <ashokgupta7090@gmail.com>
2026-03-10 00:32:48 +0530

    update folder structure, install file for formatting code

--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,5 @@
+//validation
+
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
--- src/server.js
+++ src/server.js
@@ -21,4 +21,3 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-// dfdfdfdsdfdf
